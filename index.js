56025dbda6f02fe7428d1ecfbf061bacdde0d732
Hung Tran <hungtv@MacBook-Pro-cua-Phan.local>
2022-10-02 01:03:44 +0700

    Update done code

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const imagesPath = __dirname + "/images";
 const contentPath = __dirname + "/content.txt";
 const wallpaperPath = __dirname + "/wallpaper.png";
 const { exec } = require("child_process");
-const { resolve } = require('path');
+const request = require('request');
 
 function randIndex(length) {
    return Math.floor(Math.random()*length);
+}
+
+function getRemoteContent() {
+    return new Promise((resolve, reject) => {
+        request('https://raw.githubusercontent.com/hzung/mac-auto-wallpaper/main/content.txt', { json: true }, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(body);
+        });
+    })
 }
 
 function readContent(path) {
@@ -28,41 +40,44 @@ function writeFileContent(path, content) {
 var images = fs.readdirSync(imagesPath);
 var imageRandIndex = randIndex(images.length);
 var imagePath = imagesPath + "/" + images[imageRandIndex];
-var lines = readContent(contentPath).split('\n').map(item => {
-    const tokens = item.split('|');
-    const title = tokens[0];
-    const content = tokens[1];
-    return {
-        title,
-        content
-    }
-})
-var contentRandIndex = randIndex(lines.length);
-var line = lines[contentRandIndex];
 
-var indexContent = readContent(indexSrcFilePath);
-indexContent = indexContent.replaceAll("!!IMAGE", imagePath);
-indexContent = indexContent.replaceAll("!!TITLE", line.title);
-indexContent = indexContent.replaceAll("!!CONTENT", line.content);
-writeFileContent(indexDestFilePath, indexContent);
-
-async function run() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 2400,
-        height: 1600,
-        deviceScaleFactor: 1,
-    });
+getRemoteContent().then(content => {
+    var lines = content.split('\n').map(item => {
+        const tokens = item.split('|');
+        const title = tokens[0];
+        const content = tokens[1];
+        return {
+            title,
+            content
+        }
+    })
+    var contentRandIndex = randIndex(lines.length);
+    var line = lines[contentRandIndex];
     
-    await page.goto("file://" + indexDestFilePath);
-    await page.screenshot({path: wallpaperPath});
-    await browser.close();
-}
-
-run().then(() => {
-    const command = `osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"` + wallpaperPath + `\"'`;
-    exec(command, (error, stdout, stderr) => {
-        console.log("Done: ", error, stdout, stderr)
+    var indexContent = readContent(indexSrcFilePath);
+    indexContent = indexContent.replaceAll("!!IMAGE", imagePath);
+    indexContent = indexContent.replaceAll("!!TITLE", line.title);
+    indexContent = indexContent.replaceAll("!!CONTENT", line.content);
+    writeFileContent(indexDestFilePath, indexContent);
+    
+    async function run() {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: 2400,
+            height: 1600,
+            deviceScaleFactor: 1,
+        });
+        
+        await page.goto("file://" + indexDestFilePath);
+        await page.screenshot({path: wallpaperPath});
+        await browser.close();
+    }
+    
+    run().then(() => {
+        const command = `osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"` + wallpaperPath + `\"'`;
+        exec(command, (error, stdout, stderr) => {
+            console.log("Done")
+        });
     });
-});
+})
