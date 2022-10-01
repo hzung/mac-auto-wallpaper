aea55e7da4c9721dd7eb2704e2228aabc77ee169
Hung Tran <hungtv@MacBook-Pro-cua-Phan.local>
2022-10-02 00:54:50 +0700

    Update code

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
-const indexFilePath = __dirname + "/index.html";
+const indexSrcFilePath = __dirname + "/index_src.html";
+const indexDestFilePath = __dirname + "/index_dest.html";
 const imagesPath = __dirname + "/images";
 const contentPath = __dirname + "/content.txt";
+const wallpaperPath = __dirname + "/wallpaper.png";
+const { exec } = require("child_process");
+const { resolve } = require('path');
 
 function randIndex(length) {
    return Math.floor(Math.random()*length);
@@ -17,9 +21,13 @@ function readContent(path) {
     }
 }
 
+function writeFileContent(path, content) {
+    fs.writeFileSync(path, content);
+}
+
 var images = fs.readdirSync(imagesPath);
 var imageRandIndex = randIndex(images.length);
-var imagePath = __dirname + "/" + images[imageRandIndex];
+var imagePath = imagesPath + "/" + images[imageRandIndex];
 var lines = readContent(contentPath).split('\n').map(item => {
     const tokens = item.split('|');
     const title = tokens[0];
@@ -31,7 +39,12 @@ var lines = readContent(contentPath).split('\n').map(item => {
 })
 var contentRandIndex = randIndex(lines.length);
 var line = lines[contentRandIndex];
-console.log(line);
+
+var indexContent = readContent(indexSrcFilePath);
+indexContent = indexContent.replaceAll("!!IMAGE", imagePath);
+indexContent = indexContent.replaceAll("!!TITLE", line.title);
+indexContent = indexContent.replaceAll("!!CONTENT", line.content);
+writeFileContent(indexDestFilePath, indexContent);
 
 async function run() {
     const browser = await puppeteer.launch();
@@ -42,11 +55,14 @@ async function run() {
         deviceScaleFactor: 1,
     });
     
-    await page.goto("file://" + indexFilePath);
-    await page.screenshot({path: "wallpaper.png"});
+    await page.goto("file://" + indexDestFilePath);
+    await page.screenshot({path: wallpaperPath});
     await browser.close();
 }
 
 run().then(() => {
-    console.log("Done")
-})
+    const command = `osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"` + wallpaperPath + `\"'`;
+    exec(command, (error, stdout, stderr) => {
+        console.log("Done: ", error, stdout, stderr)
+    });
+});
