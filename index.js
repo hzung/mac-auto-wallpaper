a75bd11d164176a8194286281281c1e4878d51fa
Hung Tran <hungtran.uet@gmail.com>
2022-10-04 10:06:15 +0700

    Refactor layout

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ function getRemoteContent() {
     })
 }
 
+function getRemoteAbbr() {
+    return new Promise((resolve, reject) => {
+        request('https://raw.githubusercontent.com/hzung/mac-auto-wallpaper/main/abbr.txt', { json: true }, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(body);
+        });
+    })
+}
+
 function readContent(path) {
     try {  
         var data = fs.readFileSync(path, 'utf8');
@@ -48,57 +60,63 @@ function result_contain(index) {
     return images.length > 0;
 }
 
-getRemoteContent().then(content => {
-    var lines = content.split('\n').filter(item => item.split('|').length == 2).map(item => {
-        const tokens = item.split('|');
-        const title = tokens[0];
-        const content = tokens[1];
-        return {
-            title,
-            content
+Promise.all(
+    [
+        getRemoteContent(),
+        getRemoteAbbr()
+    ]).then((values) => 
+    {
+        let termContent = values[0];
+        var terms = termContent.split('\n').filter(item => item.split('|').length == 2).map(item => {
+            const tokens = item.split('|');
+            const title = tokens[0];
+            const content = tokens[1];
+            return {
+                title,
+                content
+            }
+        })
+        
+        var imgs = result_images();
+        if (terms.length <= imgs.length) {
+            imgs.forEach(img => {
+                fs.rmSync(__dirname + "/" + img);
+            });
         }
-    })
-    
-    var imgs = result_images();
-    if (lines.length <= imgs.length) {
-        imgs.forEach(img => {
-            fs.rmSync(__dirname + "/" + img);
-        });
-    }
-    var contentRandIndex = randIndex(lines.length);
-    while (result_contain(contentRandIndex)) {
-        contentRandIndex = randIndex(lines.length);
-    }
-    
-    var line = lines[contentRandIndex];
-    var wallpaperPath = __dirname + "/wallpaper_" + contentRandIndex + ".png";
-
-    var indexContent = readContent(indexSrcFilePath);
-    indexContent = indexContent.replaceAll("!!IMAGE", imagePath);
-    indexContent = indexContent.replaceAll("!!TITLE", line.title);
-    indexContent = indexContent.replaceAll("!!CONTENT", line.content);
-    writeFileContent(indexDestFilePath, indexContent);
-    
-    async function run() {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.setViewport({
-            width: 2400,
-            height: 1600,
-            deviceScaleFactor: 1,
-        });
+        var contentRandIndex = randIndex(terms.length);
+        while (result_contain(contentRandIndex)) {
+            contentRandIndex = randIndex(terms.length);
+        }
         
-        await page.goto("file://" + indexDestFilePath);
-        
-        await page.screenshot({path: wallpaperPath});
-        await browser.close();
-    }
+        var line = terms[contentRandIndex];
+        var wallpaperPath = __dirname + "/wallpaper_" + contentRandIndex + ".png";
     
-    run().then(() => {
-        const command = `osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"` + wallpaperPath + `\"'`;
-        console.log("Execute command: " + command)
-        exec(command, (error, stdout, stderr) => {
-            console.log("Done")
+        var indexContent = readContent(indexSrcFilePath);
+        indexContent = indexContent.replaceAll("!!IMAGE", imagePath);
+        indexContent = indexContent.replaceAll("!!TITLE", line.title);
+        indexContent = indexContent.replaceAll("!!CONTENT", line.content);
+        writeFileContent(indexDestFilePath, indexContent);
+        
+        async function run() {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.setViewport({
+                width: 2400,
+                height: 1600,
+                deviceScaleFactor: 1,
+            });
+            
+            await page.goto("file://" + indexDestFilePath);
+            
+            await page.screenshot({path: wallpaperPath});
+            await browser.close();
+        }
+        
+        run().then(() => {
+            const command = `osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"` + wallpaperPath + `\"'`;
+            console.log("Execute command: " + command)
+            exec(command, (error, stdout, stderr) => {
+                console.log("Done")
+            });
         });
     });
-})
